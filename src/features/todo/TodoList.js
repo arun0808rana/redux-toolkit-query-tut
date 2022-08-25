35249c3e1571c5238164fecafed818eb34bbdc62
dev <arun0808rana@gmail.com>
2022-08-25 10:09:22 +0530

    add todos fn added, reflecting in BE but not on FE

--- src/features/todo/TodoList.js
+++ src/features/todo/TodoList.js
@@ -1,7 +1,11 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useGetTodosQuery, useAddTodoMutation, useUpdateTodoMutation, useDeleteTodoMutation } from '../api/apiSlice';
 
 function TodoList() {
+    // local states
+    const [newTodo, setNewTodo] = useState({});
+
+    // hooks
     const {
         // renaming data as todos
         data: todos,
@@ -10,19 +14,43 @@ function TodoList() {
         isError,
         error,
     } = useGetTodosQuery();
-    console.log(todos, 'todos')
+    const [addTodo] = useAddTodoMutation();
+
+    // add todo
+    const handleAddTodo = () => {
+        addTodo({
+            "id": uuidv4(),
+            "title": newTodo,
+            "completed": false
+        })
+        setNewTodo({})
+    }
+
+    // utility functions
+    function uuidv4() {
+        return ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, c =>
+            (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
+        );
+    }
+
     return (
-        <div>{
-            todos.map(todo => {
-                return (
-                    <div key={todo.id}>
-                        <li>{todo.title}</li>
-                        <button>Delete</button>
-                        <button>Edit</button>
-                    </div>
-                )
-            })
-        }</div>
+        <div>
+            <div className="flex">
+                <input type="text" onChange={(e) => setNewTodo(e.target.value)} />
+                <button onClick={handleAddTodo}>Add</button>
+            </div>
+            <div>{todos ?
+                todos.map(todo => {
+                    return (
+                        <div key={todo.id} className='todo-container'>
+                            <li>{todo.title}</li>
+                            <button>Delete</button>
+                            <button>Edit</button>
+                        </div>
+                    )
+                }) : 'Loading...'
+            }</div>
+        </div>
     )
 }
 
